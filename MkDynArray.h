1c9f67b159afabe862cf676b9e01e849c90b8b42
Marvin Kipping <zirkelkoenig@users.noreply.github.com>
2023-04-07 19:54:14 +0200

    C++ compatibility removed because fuck that language

--- MkDynArray.h
+++ MkDynArray.h
@@ -17,65 +17,54 @@ Usage:
 #ifndef MK_DYN_ARRAY
 #define MK_DYN_ARRAY
 
-#ifndef __cplusplus
 #include <stddef.h>
-#endif
 
 typedef void * (*MkDynArray_MemAlloc_Cb)(size_t size);
 typedef void * (*MkDynArray_MemRealloc_Cb)(void * pointer, size_t size);
 typedef void (*MkDynArray_MemFree_Cb)(void * pointer);
 typedef void (*MkDynArray_MemCopy_Cb)(void * dest, const void * source, size_t size);
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+//-------------------
+// MEMORY MANAGEMENT
+//-------------------
 
-    //-------------------
-    // MEMORY MANAGEMENT
-    //-------------------
+extern MkDynArray_MemAlloc_Cb MkDynArray_MemAlloc;
+extern MkDynArray_MemRealloc_Cb MkDynArray_MemRealloc;
+extern MkDynArray_MemFree_Cb MkDynArray_MemFree;
+extern MkDynArray_MemCopy_Cb MkDynArray_MemCopy;
 
-    extern MkDynArray_MemAlloc_Cb MkDynArray_MemAlloc;
-    extern MkDynArray_MemRealloc_Cb MkDynArray_MemRealloc;
-    extern MkDynArray_MemFree_Cb MkDynArray_MemFree;
-    extern MkDynArray_MemCopy_Cb MkDynArray_MemCopy;
+//-------------------
+// PUBLIC INTERFACE
+//-------------------
 
-    //-------------------
-    // PUBLIC INTERFACE
-    //-------------------
+// T * MkDynArray_Create(T, const size_t capacity)
+//
+// Create a dynamic array of a given type and an initial capacity. The capacity will also act as the grow count.
+// NOTE: You are not allowed to pass 0 as the capacity!
+// Returns NULL if memory allocation failed.
+#define MkDynArray_Create(T, capacity) (T *)MkDynArray_Create_Impl(sizeof(T), capacity);
 
-    // T * MkDynArray_Create(T, const size_t capacity)
-    //
-    // Create a dynamic array of a given type and an initial capacity. The capacity will also act as the grow count.
-    // NOTE: You are not allowed to pass 0 as the capacity!
-    // Returns NULL if memory allocation failed.
-    #define MkDynArray_Create(T, capacity) (T *)MkDynArray_Create_Impl(sizeof(T), capacity);
+// void MkDynArray_Destroy(T * array)
+// 
+// Destroy a dynamic array.
+void MkDynArray_Destroy(void * array);
 
-    // void MkDynArray_Destroy(T * array)
-    // 
-    // Destroy a dynamic array.
-    void MkDynArray_Destroy(void * array);
+// size_t MkDynArray_Count(T * array)
+// 
+// Get the current element count of the array.
+size_t MkDynArray_Count(void * array);
 
-    // size_t MkDynArray_Count(T * array)
-    // 
-    // Get the current element count of the array.
-    size_t MkDynArray_Count(void * array);
+// int MkDynArray_Add(T ** array, T elem)
+//
+// Add an element to the end of the dynamic array, resizing it if necessary.
+// Returns 0 if memory allocation failed.
+#define MkDynArray_Add(array, elem) MkDynArray_Increment_Impl(array) ? ((*(array))[MkDynArray_Count(*(array)) - 1] = (elem), 1) : 0
 
-    // int MkDynArray_Add(T ** array, T elem)
-    //
-    // Add an element to the end of the dynamic array, resizing it if necessary.
-    // Returns 0 if memory allocation failed.
-    #define MkDynArray_Add(array, elem) MkDynArray_Increment_Impl(array) ? ((*(array))[MkDynArray_Count(*(array)) - 1] = (elem), 1) : 0
+//--------------------------
+// IMPLEMENTATION
+//--------------------------
 
-    //--------------------------
-    // IMPLEMENTATION
-    //--------------------------
-
-    void * MkDynArray_Create_Impl(const size_t elemSize, const size_t capacity);
-    int MkDynArray_Increment_Impl(void ** array);
-
-#ifdef __cplusplus
-}
-#endif
+void * MkDynArray_Create_Impl(const size_t elemSize, const size_t capacity);
+int MkDynArray_Increment_Impl(void ** array);
 
 #endif
--- MkString.h
+++ MkString.h
@@ -14,9 +14,7 @@ Usage:
 #ifndef MK_STRING
 #define MK_STRING
 
-#ifndef __cplusplus
 #include <wchar.h>
-#endif
 
 typedef unsigned char byte;
 
@@ -35,77 +33,68 @@ typedef void * (*MkString_MemRealloc_Cb)(void * pointer, size_t size);
 typedef void (*MkString_MemFree_Cb)(void * pointer);
 typedef void (*MkString_MemCopy_Cb)(void * dest, const void * source, size_t size);
 
-#ifdef __cplusplus
-extern "C"
+//-------------------
+// MEMORY MANAGEMENT
+//-------------------
+
+extern MkString_MemAlloc_Cb MkString_MemAlloc;
+extern MkString_MemRealloc_Cb MkString_MemRealloc;
+extern MkString_MemFree_Cb MkString_MemFree;
+extern MkString_MemCopy_Cb MkString_MemCopy;
+
+//-------------------
+// PUBLIC INTERFACE
+//-------------------
+
+// Convert a C string into a UTF-8 byte array.
+// WARNING: Make sure the C string is valid!
+// Returns NULL if memory allocation failed.
+byte * MkString_CwToUtf8(const wchar_t * cwString, size_t * utf8Length);
+
+// Parse a UTF-8 byte array into a C string. Errors will be marked by the replacement char 0xFFFD.
+// Returns NULL if memory allocation failed.
+wchar_t * MkString_CwFromUtf8(const byte * utf8, const size_t utf8Length);
+
+// A safer wide string type.
+typedef struct
 {
-#endif
+    wchar_t * chars;
+    size_t length;
+} MkStringW;
 
-    //-------------------
-    // MEMORY MANAGEMENT
-    //-------------------
+// Create a safe string by copying a C string.
+// WARNING: Make sure the C string is valid (null-terminated)! If it isn't the usual problems will arise.
+// Returns NULL if memory allocation failed.
+MkStringW * MkStringW_Copy_Cw(const wchar_t * cwString);
 
-    extern MkString_MemAlloc_Cb MkString_MemAlloc;
-    extern MkString_MemRealloc_Cb MkString_MemRealloc;
-    extern MkString_MemFree_Cb MkString_MemFree;
-    extern MkString_MemCopy_Cb MkString_MemCopy;
+// Create a copy.
+// Returns NULL if memory allocation failed.
+MkStringW * MkStringW_Copy(const MkStringW * string);
 
-    //-------------------
-    // PUBLIC INTERFACE
-    //-------------------
+// Create a safe string by concatenating two C strings.
+// WARNING: Make sure the C strings are valid!
+// Returns NULL if memory allocation failed.
+MkStringW * MkStringW_Concat_Cw(const wchar_t * cwStringA, const wchar_t * cwStringB);
 
-    // Convert a C string into a UTF-8 byte array.
-    // WARNING: Make sure the C string is valid!
-    // Returns NULL if memory allocation failed.
-    byte * MkString_CwToUtf8(const wchar_t * cwString, size_t * utf8Length);
+// Destroy a safe string.
+void MkStringW_Destroy(MkStringW * string);
 
-    // Parse a UTF-8 byte array into a C string. Errors will be marked by the replacement char 0xFFFD.
-    // Returns NULL if memory allocation failed.
-    wchar_t * MkString_CwFromUtf8(const byte * utf8, const size_t utf8Length);
+// Append source C string to the end of dest.
+// WARNING: Make sure the C string is valid!
+// Returns 0 if memory allocation failed.
+int MkStringW_Append_Cw(MkStringW * dest, const wchar_t * source);
 
-    // A safer wide string type.
-    typedef struct
-    {
-        wchar_t * chars;
-        size_t length;
-    } MkStringW;
+// Append a C string to a Win32 folder path, inserting a separator ('\\') if necessary.
+// WARNING: Make sure the C string is valid!
+// Returns 0 if memory allocation failed.
+int MkStringW_AppendWin32Path_Cw(MkStringW * win32Path, const wchar_t * component);
 
-    // Create a safe string by copying a C string.
-    // WARNING: Make sure the C string is valid (null-terminated)! If it isn't the usual problems will arise.
-    // Returns NULL if memory allocation failed.
-    MkStringW * MkStringW_Copy_Cw(const wchar_t * cwString);
+// Convert a string into a UTF-8 byte array.
+// Returns NULL if memory allocation failed.
+byte * MkStringW_ToUtf8(const MkStringW * string, size_t * utf8Length);
 
-    // Create a copy.
-    // Returns NULL if memory allocation failed.
-    MkStringW * MkStringW_Copy(const MkStringW * string);
-
-    // Create a safe string by concatenating two C strings.
-    // WARNING: Make sure the C strings are valid!
-    // Returns NULL if memory allocation failed.
-    MkStringW * MkStringW_Concat_Cw(const wchar_t * cwStringA, const wchar_t * cwStringB);
-
-    // Destroy a safe string.
-    void MkStringW_Destroy(MkStringW * string);
-
-    // Append source C string to the end of dest.
-    // WARNING: Make sure the C string is valid!
-    // Returns 0 if memory allocation failed.
-    int MkStringW_Append_Cw(MkStringW * dest, const wchar_t * source);
-
-    // Append a C string to a Win32 folder path, inserting a separator ('\\') if necessary.
-    // WARNING: Make sure the C string is valid!
-    // Returns 0 if memory allocation failed.
-    int MkStringW_AppendWin32Path_Cw(MkStringW * win32Path, const wchar_t * component);
-
-    // Convert a string into a UTF-8 byte array.
-    // Returns NULL if memory allocation failed.
-    byte * MkStringW_ToUtf8(const MkStringW * string, size_t * utf8Length);
-
-    // Parse a UTF-8 byte array into a string. Errors will be marked by the replacement char 0xFFFD.
-    // Returns NULL if memory allocation failed.
-    MkStringW * MkStringW_FromUtf8(const byte * utf8, const size_t utf8Length);
-
-#ifdef __cplusplus
-}
-#endif
+// Parse a UTF-8 byte array into a string. Errors will be marked by the replacement char 0xFFFD.
+// Returns NULL if memory allocation failed.
+MkStringW * MkStringW_FromUtf8(const byte * utf8, const size_t utf8Length);
 
 #endif
