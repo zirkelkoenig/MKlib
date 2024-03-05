43f059d0b7c529a23dfbe88196076f2a511b81b4
Marvin Kipping <zirkelkoenig@users.noreply.github.com>
2024-03-05 21:46:11 +0100

    List: bit the bullet and switched to shitty C++ templates, because type safety is actually pretty good

--- MkList.h
+++ MkList.h
@@ -28,46 +28,108 @@ SOFTWARE.
 #include <assert.h>
 #include <limits.h>
 
-typedef unsigned char byte;
+//typedef unsigned char byte;
+typedef unsigned int uint;
+typedef unsigned short ushort;
 typedef unsigned long ulong;
 
 // A dynamic array.
-struct MkList {
-    ulong count;
-    ulong capacity;
-    ulong growCount;
-    ulong elemSize;
-    void * elems;
+template<class T>
+struct MkList2 {
+    ulong count; // the current number of elements in the array
+    ulong capacity; // the current array capacity
+    ulong growCount; // the number of element slots to add when expanding the array
+    T * elems; // the actual element slots
+
+    // Initialize the array.
+    MkList2(ulong growCount) {
+        Init(growCount);
+    }
+
+    // Initialize the array.
+    void Init(ulong growCount) {
+        assert(growCount != 0);
+
+        count = 0;
+        capacity = 0;
+        this->growCount = growCount;
+        elems = nullptr;
+    }
+
+    void Assert() {
+        assert(count <= capacity);
+        assert(growCount != 0);
+        assert(elems || capacity == 0);
+    }
+
+    // Clear the array, freeing all its allocated memory.
+    void Clear() {
+        Assert();
+
+        count = 0;
+        capacity = 0;
+        if (elems) {
+            free(elems);
+        }
+    }
+
+    // Explicitly set the array capacity.
+    // Returns FALSE on memory allocation failure.
+    bool SetCapacity(ulong newCapacity) {
+        Assert();
+
+        T * newElems = static_cast<T *>(realloc(elems, newCapacity * sizeof(T)));
+        if (!(newElems || newCapacity == 0)) {
+            return false;
+        }
+        if (count > newCapacity) {
+            count = newCapacity;
+        }
+        capacity = newCapacity;
+        elems = newElems;
+        return true;
+    }
+
+    // Create space for a number of new elements, shifting the successive elements.
+    // Pass ULONG_MAX as the index to append the new elements to the end of the array.
+    // Returns a pointer to the first new element slot or NULL on memory allocation failure.
+    T * Insert(ulong index, ulong shiftCount) {
+        Assert();
+        assert(index <= count || index == ULONG_MAX);
+
+        ulong newCount = count + shiftCount;
+        if (newCount > capacity) {
+            ulong newCapacity = ((newCount / growCount) + 1) * growCount;
+            if (!SetCapacity(newCapacity)) {
+                return nullptr;
+            }
+        }
+
+        if (index == ULONG_MAX) {
+            index = count;
+        }
+        count = newCount;
+
+        ulong shiftEnd = index + shiftCount;
+        for (ulong i = count - 1; i >= shiftEnd; i--) {
+            elems[i] = elems[i - shiftCount];
+        }
+
+        return &elems[index];
+    }
+
+    // Remove elements from the array, unshifting the successive elements.
+    void Remove(ulong index, ulong unshiftCount) {
+        Assert();
+        assert(index < count);
+        assert(unshiftCount <= count - index);
+
+        ulong shiftEnd = count - unshiftCount;
+        for (ulong i = index; i != shiftEnd; i++) {
+            elems[i] = elems[i + unshiftCount];
+        }
+        count -= unshiftCount;
+    }
 };
-
-// Initialize a list.
-void MkListInit(MkList * listPtr, ulong growCount, ulong elemSize);
-
-// Clear a list, freeing its allocated memory.
-void MkListClear(MkList * listPtr);
-
-// Explicitly set the capacity of a list.
-// Returns FALSE on memory allocation failure.
-bool MkListSetCapacity(MkList * listPtr, ulong capacity);
-
-// Insert a number of new element slots at the given index, shifting the successive elements. Pass ULONG_MAX as the index to append the new elements at the end of the list.
-// Returns a pointer to the first new element slot or NULL on memory allocation failure.
-void * MkListInsert(MkList * listPtr, ulong index, ulong count);
-
-// Check if a list is valid. Will only perform in debug builds.
-static void MkListAssert(MkList * listPtr) {
-    assert(listPtr);
-    assert(listPtr->count <= listPtr->capacity);
-    assert(listPtr->growCount != 0);
-    assert(listPtr->elemSize != 0);
-    assert(listPtr->elems != nullptr || listPtr->capacity == 0);
-}
-
-// Get a pointer the the element slot at the given index.
-static void * MkListGet(MkList * listPtr, ulong index) {
-    MkListAssert(listPtr);
-    assert(index < listPtr->count);
-    return static_cast<byte *>(listPtr->elems) + (index * listPtr->elemSize);
-}
 
 #endif
